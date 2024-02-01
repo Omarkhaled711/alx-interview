032ebff25559343e9649ca6f7ddcca6b1e5345a2
Omar Khaled <o.khaled6789@gmail.com>
2024-02-01 22:38:50 +0200

    edited a typo

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,17 +14,17 @@ def validUTF8(data):
     cont = 0
     i = 0
     while i < len(data):
-        if data[i] >> 5 == 0xb110:
+        if data[i] >> 5 == 0b110:
             cont = 1
-        elif data[i] >> 4 == 0xb1110:
+        elif data[i] >> 4 == 0b1110:
             cont = 2
-        elif data[i] >> 3 == 0xb11110:
+        elif data[i] >> 3 == 0b11110:
             cont = 3
         elif data[i] >> 7 != 0:
             return False
         i += 1
         while cont != 0:
-            if i >= len(data) or data[i] >> 6 != 0xb10:
+            if i >= len(data) or data[i] >> 6 != 0b10:
                 return False
             cont -= 1
             i += 1
