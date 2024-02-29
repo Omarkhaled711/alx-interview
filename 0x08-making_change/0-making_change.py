219d4773a6e0955374b8af2e0f3c93e736302838
Omar Khaled <o.khaled6789@gmail.com>
2024-02-29 03:25:39 +0200

    used tapulation instead of memoization to pass the efficiency test

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,28 +7,6 @@ solving Making Change problem module
 import sys
 
 
-def calculate_change(coins, total, dp, coin_num):
-    """
-    using memoization to calculate the fewest number of coins
-    """
-    if total == 0:
-        return 0
-
-    if dp[total] != -1:
-        return dp[total]
-
-    result = sys.maxsize
-
-    for i in range(coin_num):
-        if (coins[i] <= total):
-            take_coin = calculate_change(coins, total-coins[i], dp, coin_num)
-
-            if take_coin != sys.maxsize and result > take_coin + 1:
-                result = take_coin + 1
-    dp[total] = result
-    return result
-
-
 def makeChange(coins, total):
     """
     Given a pile of coins of different values, determine the
@@ -36,9 +14,15 @@ def makeChange(coins, total):
     """
     if (total <= 0):
         return 0
-
-    dp = [-1]*(total + 1)
-    result = calculate_change(coins, total, dp, len(coins))
-    if result == sys.maxsize:
+    if coins is None or not coins:
         return -1
-    return result
+    dp = [sys.maxsize] * (total + 1)
+    dp[0] = 0
+    for coin in coins:
+        for i in range(coin, total + 1):
+            take_coin = dp[i - coin]
+            if take_coin != sys.maxsize and take_coin + 1 < dp[i]:
+                dp[i] = take_coin + 1
+    if dp[total] == sys.maxsize:
+        return -1
+    return dp[total]
